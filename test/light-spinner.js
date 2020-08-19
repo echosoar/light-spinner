13ebcdc6be338df8b1de5e901511b8fc93ae6724
echosoar <82163514@qq.com>
2020-08-19 23:30:02 +0800

    docs: spin gif

--- test/light-spinner.js
+++ test/light-spinner.js
@@ -1,16 +1,24 @@
 const spinClass = require('../dist');
+const wait = async (timeout) => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
 (async () => {
   const spin = new spinClass.default({
     text: 'test',
   });
   spin.start();
-  await new Promise((resolve) => {
-    setTimeout(resolve, 2000);
-  });
+  await wait(1000);
   // change text
   spin.text = 'test 2';
-  await new Promise((resolve) => {
-    setTimeout(resolve, 2000);
-  });
+  
+  const list = new Array(100).fill(0);
+  for(let i in list) {
+    await wait(Math.random() * 100);
+    spin.text = `[${ i }/${list.length}] install`;
+  }
+  spin.text = 'complete!';
+  await wait(1000);
   spin.stop();
 })();
