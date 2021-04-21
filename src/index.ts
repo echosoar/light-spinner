8b22b088d3c62fada4ed17ef63ce2032cc43179b
echosoar <82163514@qq.com>
2021-04-21 17:05:43 +0800

    fix: release 1.0.4

--- src/index.ts
+++ src/index.ts
@@ -28,33 +28,22 @@ export default class LightSpinner {
 
   // https://en.wikipedia.org/wiki/ANSI_escape_code
   private clearLine() {
-    const messageLine = (this.message || '').split('\n');
-    while (messageLine.length) {
-      const message = messageLine.pop();
-      const clearChar = '\u001b[2K'; // use 'CSI n K' erase in line : \x01[nK
-      const moveCursor = `\u001b[${Buffer.byteLength(message)}D`; // use 'CSI n D' cursor back : \x01[nD
-      if (!this.isWin) {
-        process.stdout.write('\u001b[u');
-      }
-      process.stdout.write(clearChar);
-      process.stdout.write(moveCursor);
-      if (messageLine.length) {
-        // 跳到上一行
-        process.stdout.write('\u001b[1F');
-      }
+    if (!this.message) {
+      return;
     }
+    const clearChar = '\u001b[2K'; // use 'CSI n K' erase in line : \x01[nK
+    const moveCursor = `\u001b[${Buffer.byteLength(this.message)}D`; // use 'CSI n D' cursor back : \x01[nD
+    process.stdout.write(clearChar);
+    process.stdout.write(moveCursor);
   }
 
   private output(message: string) {
     this.message = message;
     process.stdout.write(message);
-    if (!this.isWin) {
-      process.stdout.write('\u001b[s');
-    }
   }
 
   private doing() {
-    const message = `${this.spinners[this.index ++]} ${this.text || ''}${ this.isWin ? ' ' : '\n'}`;
+    const message = `${this.spinners[this.index ++]} ${this.text || ''} `;
     this.clearLine();
     this.output(message);
     if (this.index >= this.spinners.length) {
